a7ec64e541a1b7e55cad692126ec8162af26184d
Guillaume Moigneu <n@nls.io>
2016-09-18 23:08:43 +0200

    Change tmp path

--- configuration.php
+++ configuration.php
@@ -56,8 +56,8 @@ class JConfig {
 	public $unicodeslugs = '0';
 	public $feed_limit = '10';
 	public $feed_email = 'none';
-	public $log_path = '/code/administrator/logs';
-	public $tmp_path = '/code/tmp';
+	public $log_path = '/app/administrator/logs';
+	public $tmp_path = '/app/tmp';
 	public $lifetime = '15';
 	public $session_handler = 'database';
 
